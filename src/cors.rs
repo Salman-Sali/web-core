04de0697cc8476940c58ba252beae292233f7a4f
Salman-Sali <emailsalmansalih@gmail.com>
2026-03-04 00:03:33 +0530

    ugh. cors.

--- src/cors.rs
+++ src/cors.rs
@@ -1,8 +1,9 @@
 use axum::Router;
-use tower_http::cors::CorsLayer;
 use axum::http::{
     HeaderValue, Method,
-    header::{ACCEPT, AUTHORIZATION, CONTENT_TYPE}};
+    header::{ACCEPT, AUTHORIZATION, CONTENT_TYPE},
+};
+use tower_http::cors::CorsLayer;
 
 use crate::web_core::WebCoreState;
 
@@ -10,33 +11,41 @@ pub trait WithCorsLayer {
     fn with_cors_layer(self, frontend_url: Option<String>) -> Self;
 }
 
-impl<T> WithCorsLayer for Router<WebCoreState<T>> where T: Clone + Send + Sync + 'static {
+impl<T> WithCorsLayer for Router<WebCoreState<T>>
+where
+    T: Clone + Send + Sync + 'static,
+{
     fn with_cors_layer(self, frontend_url: Option<String>) -> Self {
-        self
-            .layer(generate_cors(frontend_url))
+        self.layer(generate_cors(frontend_url))
     }
 }
 
-
-
-pub fn generate_cors(frontend_url: Option<String>) -> CorsLayer {
+pub fn generate_cors(mut frontend_url: Option<String>) -> CorsLayer {
     #[allow(unused_mut)]
     let mut cors = CorsLayer::new()
-    .allow_credentials(false)
-    .allow_headers([ACCEPT, AUTHORIZATION, CONTENT_TYPE])
-    .allow_methods([Method::POST, Method::PUT, Method::DELETE, Method::GET]);
+        .allow_credentials(false)
+        .allow_headers([ACCEPT, AUTHORIZATION, CONTENT_TYPE])
+        .allow_methods([
+            Method::POST,
+            Method::PUT,
+            Method::DELETE,
+            Method::GET,
+            Method::OPTIONS,
+        ]);
 
     if let Some(frontend_url) = &frontend_url {
-        cors = cors
-            .allow_origin(frontend_url.parse::<HeaderValue>().unwrap());        
+        cors = cors.allow_origin(
+            frontend_url
+                .trim_end_matches("/")
+                .parse::<HeaderValue>()
+                .unwrap(),
+        );
     }
 
     #[cfg(feature = "local")]
     {
         use tower_http::cors::Any;
-        cors = cors
-            .allow_origin(Any)
-            .allow_credentials(false);
+        cors = cors.allow_origin(Any).allow_credentials(false);
     }
     return cors;
 }
